83b1716684ae00b75c5f7fef889c2004c4122360
Jaishree <jaishree.ct21@bitsathy.ac.in>
2023-04-21 10:35:39 +0530

    update

---
+++ events/forms.js
@@ -0,0 +1,24 @@
+function inovaForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfzAq_4E0V5jfmQNyL56W1vwhNATATih6BpC3AbmdXghsgxpg/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function codeForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLScnUy73qmDZvkQI4XS0LHehM0OOQtV3nNGamGTipJ9Au8uikw/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function uiForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdPlZy3QHWakBVBdoKIcyD5GielNjqGktkR9vI7tbyKcgUf8w/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function pitchForm() {
+  var formUrl = "https://docs.google.com/";
+  window.open(formUrl);
+}
+function frontForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdOrU6tBudlLVQYf4jwaALAFuAl8xwckcFv5siP933jRaw3bw/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function nontechForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdWlRkpZ1wClRRbJsj_4JS1FHRPmZhTHSvFFJTMiK3kapuE0A/viewform?usp=sf_link";
+  window.open(formUrl);
+}
